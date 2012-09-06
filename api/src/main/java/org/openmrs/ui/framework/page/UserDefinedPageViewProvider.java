ac2dc314b805fc3b4a5e88d4866658722187012a
wluyima <wyclif@openmrs.org>
2012-09-06 15:20:42 -0400

    Allow User-defined pages to be stored in the database - UIFR-70

---
+++ api/src/main/java/org/openmrs/ui/framework/page/UserDefinedPageViewProvider.java
@@ -0,0 +1,72 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.ui.framework.page;
+
+import groovy.text.SimpleTemplateEngine;
+import groovy.text.Template;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.ui.framework.UserDefinedPageView;
+import org.openmrs.ui.framework.db.UserDefinedPageViewDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+/**
+ * A Groovy {@link PageViewProvider} for user defined pages, it looks up views from the database
+ */
+public class UserDefinedPageViewProvider implements PageViewProvider {
+	
+	private static final Log log = LogFactory.getLog(UserDefinedPageViewProvider.class);
+	
+	// internal data
+	SimpleTemplateEngine engine = new SimpleTemplateEngine();
+	
+	@Autowired
+	@Qualifier("userDefinedPageviewDAO")
+	UserDefinedPageViewDAO dao;
+	
+	@Override
+	public PageView getView(String name) {
+		
+		if (log.isDebugEnabled())
+			log.debug("Looking up Page view for: '" + name + "'");
+		
+		try {
+			String controllerName = null;
+			
+			UserDefinedPageView userDefined = dao.getPageViewByName(name);
+			if (userDefined == null)
+				return null;
+			
+			String definition = userDefined.getTemplateText();
+			// <!--CONTROLLER:{non-whitespace, not greedy}--> allowing for whitespaces between elements
+			Pattern p = Pattern.compile("<!--\\s*CONTROLLER\\s*:\\s*(\\S*?)\\s*-->");
+			Matcher m = p.matcher(definition);
+			if (m.find())
+				controllerName = m.group(1);
+			Template template = engine.createTemplate(definition);
+			//TODO, Typically we should be checking for the template type
+			//and return the appropriate PageView implementation
+			return new GroovyPageView(template, controllerName);
+		}
+		catch (Exception ex) {
+			log.error("Error creating GroovyPageView", ex);
+			throw new RuntimeException("Error creating GroovyPageView", ex);
+		}
+	}
+}
