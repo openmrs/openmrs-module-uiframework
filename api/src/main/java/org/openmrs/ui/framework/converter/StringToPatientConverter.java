46a14f075cc7c8f4ebd6e2beec06b53f9fe9cc0c
djazayeri <jazayeri@alum.mit.edu>
2013-06-04 16:38:06 -0700

    UIFR-111 - StringToPatientConverter should support finding a patient by id or uuid

--- api/src/main/java/org/openmrs/ui/framework/converter/StringToPatientConverter.java
+++ api/src/main/java/org/openmrs/ui/framework/converter/StringToPatientConverter.java
@@ -2,16 +2,32 @@ package org.openmrs.ui.framework.converter;
 
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.Patient;
-import org.openmrs.api.context.Context;
+import org.openmrs.api.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 
+import java.util.regex.Pattern;
+
 public class StringToPatientConverter implements Converter<String, Patient> {
-	
+
+    private Pattern onlyDigits = Pattern.compile("\\d+");
+
+    @Autowired
+    private PatientService patientService;
+
+    public void setPatientService(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
 	@Override
 	public Patient convert(String id) {
-		if (StringUtils.isBlank(id))
+		if (StringUtils.isBlank(id)) {
 			return null;
-		return Context.getPatientService().getPatient(Integer.valueOf(id));
+        } else if (onlyDigits.matcher(id).matches()) {
+            return patientService.getPatient(Integer.valueOf(id));
+        }else {
+            return patientService.getPatientByUuid(id);
+        }
 	}
-	
+
 }
---
+++ api/src/test/java/org/openmrs/ui/framework/converter/StringToPatientConverterTest.java
@@ -0,0 +1,51 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.ui.framework.converter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.api.PatientService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ *
+ */
+public class StringToPatientConverterTest {
+
+    private StringToPatientConverter converter;
+    private PatientService patientService;
+
+    @Before
+    public void setUp() throws Exception {
+        patientService = mock(PatientService.class);
+        converter = new StringToPatientConverter();
+        converter.setPatientService(patientService);
+    }
+
+    @Test
+    public void testConvertByPatientId() throws Exception {
+        converter.convert("1234");
+        verify(patientService).getPatient(1234);
+    }
+
+    @Test
+    public void testConvertByPatientUuid() throws Exception {
+        String uuid = "8d793bee-c2cc-11de-8d13-0010c6dffd0f";
+        converter.convert(uuid);
+        verify(patientService).getPatientByUuid(uuid);
+    }
+}
