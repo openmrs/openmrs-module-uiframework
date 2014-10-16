5702f153dee550d55bb8f5fc68a707e812391ec1
wyclif <wyclif@openmrs.org>
2014-10-16 18:06:51 -0400

    Renamed StringToCodedValueOrFreeTextConverter to StringToCodedValueOrFreeTextConverter and changed its structure

--- api/src/main/java/org/openmrs/ui/framework/converter/StringToCodedOrFreeTextValueConverter.java
+++ api/src/main/java/org/openmrs/ui/framework/converter/StringToCodedOrFreeTextValueConverter.java
@@ -13,25 +13,25 @@
  */
 package org.openmrs.ui.framework.converter;
 
+import static org.openmrs.ui.framework.CodedOrFreeTextValue.CONCEPT_NAME_PREFIX;
+import static org.openmrs.ui.framework.CodedOrFreeTextValue.CONCEPT_PREFIX;
+
 import org.apache.commons.lang.StringUtils;
+import org.openmrs.Concept;
+import org.openmrs.ConceptName;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
-import org.openmrs.ui.framework.CodedValueOrFreeText;
+import org.openmrs.ui.framework.CodedOrFreeTextValue;
 import org.openmrs.ui.framework.converter.util.ConversionUtil;
 import org.springframework.core.convert.converter.Converter;
 
 /**
- * Converts a String it to a code value or returns it as a free text. The supported types it can
- * convert to are Concept and ConceptName, the string to be converted has to start with 'CONCEPT:'
- * to convert to a concept or 'CONCEPT_NAME:' to convert to a concept name otherwise it will return
- * the passed in string as the wrapped value. Note that this converter never returns null but the
+ * Converts a String to a concept name, concept, the string to be converted has to start with
+ * 'CONCEPT:' for a concept or 'CONCEPT_NAME:' for a concept name otherwise it will return the
+ * passed in string as the wrapped value. Note that this converter never returns null but the
  * wrapped value can be null.
  */
-public class StringToCodedValueOrFreeTextConverter implements Converter<String, CodedValueOrFreeText> {
-	
-	public static final String CONCEPT_PREFIX = "CONCEPT:";
-	
-	public static final String CONCEPT_NAME_PREFIX = "CONCEPT_NAME:";
+public class StringToCodedOrFreeTextValueConverter implements Converter<String, CodedOrFreeTextValue> {
 	
 	/**
 	 * @see Converter#convert(Object)
@@ -45,38 +45,40 @@ public class StringToCodedValueOrFreeTextConverter implements Converter<String, 
 	 * @should handle free text
 	 */
 	@Override
-	public CodedValueOrFreeText convert(String str) {
-		
-		Object value = null;
-		
+	public CodedOrFreeTextValue convert(String str) {
+		String nonCodedValue = null;
 		if (StringUtils.isNotBlank(str)) {
 			ConceptService cs = Context.getConceptService();
 			if (str.startsWith(CONCEPT_PREFIX)) {
 				String uuidOrConceptId = str.substring(str.indexOf(CONCEPT_PREFIX) + CONCEPT_PREFIX.length());
 				if (StringUtils.isNotBlank(uuidOrConceptId)) {
+					Concept concept;
 					if (ConversionUtil.onlyDigits(uuidOrConceptId)) {
-						value = cs.getConcept(Integer.valueOf(uuidOrConceptId));
+						concept = cs.getConcept(Integer.valueOf(uuidOrConceptId));
 					} else {
-						value = cs.getConceptByUuid(uuidOrConceptId);
+						concept = cs.getConceptByUuid(uuidOrConceptId);
 					}
+					return new CodedOrFreeTextValue(concept);
 				}
 				
 			} else if (str.startsWith(CONCEPT_NAME_PREFIX)) {
 				String uuidOrConceptNameId = str.substring(str.indexOf(CONCEPT_NAME_PREFIX) + CONCEPT_NAME_PREFIX.length());
 				
 				if (StringUtils.isNotBlank(uuidOrConceptNameId)) {
+					ConceptName name;
 					if (ConversionUtil.onlyDigits(uuidOrConceptNameId)) {
-						value = cs.getConceptName(Integer.valueOf(uuidOrConceptNameId));
+						name = cs.getConceptName(Integer.valueOf(uuidOrConceptNameId));
 					} else {
-						value = cs.getConceptNameByUuid(uuidOrConceptNameId);
+						name = cs.getConceptNameByUuid(uuidOrConceptNameId);
 					}
+					return new CodedOrFreeTextValue(name);
 				}
 				
 			} else {
-				value = str;
+				nonCodedValue = str;
 			}
 		}
 		
-		return new CodedValueOrFreeText(value);
+		return new CodedOrFreeTextValue(nonCodedValue);
 	}
 }
--- api/src/test/java/org/openmrs/ui/framework/converter/StringToCodedOrFreeTextValueConverterTest.java
+++ api/src/test/java/org/openmrs/ui/framework/converter/StringToCodedOrFreeTextValueConverterTest.java
@@ -16,6 +16,8 @@ package org.openmrs.ui.framework.converter;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
+import static org.openmrs.ui.framework.CodedOrFreeTextValue.CONCEPT_NAME_PREFIX;
+import static org.openmrs.ui.framework.CodedOrFreeTextValue.CONCEPT_PREFIX;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,92 +26,87 @@ import org.openmrs.ConceptName;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
-import org.openmrs.ui.framework.CodedValueOrFreeText;
+import org.openmrs.ui.framework.CodedOrFreeTextValue;
 
-public class StringToCodedValueOrFreeTextConverterTest extends BaseModuleContextSensitiveTest {
+public class StringToCodedOrFreeTextValueConverterTest extends BaseModuleContextSensitiveTest {
 	
 	private ConceptService cs;
 	
-	private StringToCodedValueOrFreeTextConverter converter;
+	private StringToCodedOrFreeTextValueConverter converter;
 	
 	@Before
 	public void setup() {
 		cs = Context.getConceptService();
-		converter = new StringToCodedValueOrFreeTextConverter();
+		converter = new StringToCodedOrFreeTextValueConverter();
 	}
 	
 	/**
 	 * @verifies convert a valid concept id string
-	 * @see StringToCodedValueOrFreeTextConverter#convert(String)
+	 * @see StringToCodedOrFreeTextValueConverter#convert(String)
 	 */
 	@Test
 	public void convert_shouldConvertAValidConceptIdString() throws Exception {
 		int conceptId = 3;
-		String toConvert = StringToCodedValueOrFreeTextConverter.CONCEPT_PREFIX + conceptId;
-		CodedValueOrFreeText codedValue = converter.convert(toConvert);
-		Concept concept = codedValue.getValueAsType();
+		CodedOrFreeTextValue codedValue = converter.convert(CONCEPT_PREFIX + conceptId);
+		Concept concept = codedValue.getCodedValue();
 		assertEquals(conceptId, concept.getConceptId().intValue());
 	}
 	
 	/**
 	 * @verifies convert a valid concept uuid string
-	 * @see StringToCodedValueOrFreeTextConverter#convert(String)
+	 * @see StringToCodedOrFreeTextValueConverter#convert(String)
 	 */
 	@Test
 	public void convert_shouldConvertAValidConceptUuidString() throws Exception {
 		String conceptUuid = "0cbe2ed3-cd5f-4f46-9459-26127c9265ab";
-		String toConvert = StringToCodedValueOrFreeTextConverter.CONCEPT_PREFIX + conceptUuid;
-		CodedValueOrFreeText codedValue = converter.convert(toConvert);
-		Concept concept = codedValue.getValueAsType();
+		CodedOrFreeTextValue codedValue = converter.convert(CONCEPT_PREFIX + conceptUuid);
+		Concept concept = codedValue.getCodedValue();
 		assertEquals(conceptUuid, concept.getUuid());
 	}
 	
 	/**
 	 * @verifies convert a valid concept name id string
-	 * @see StringToCodedValueOrFreeTextConverter#convert(String)
+	 * @see StringToCodedOrFreeTextValueConverter#convert(String)
 	 */
 	@Test
 	public void convert_shouldConvertAValidConceptNameIdString() throws Exception {
 		int conceptNameId = 1439;
-		String toConvert = StringToCodedValueOrFreeTextConverter.CONCEPT_NAME_PREFIX + conceptNameId;
-		CodedValueOrFreeText codedValue = converter.convert(toConvert);
-		ConceptName name = codedValue.getValueAsType();
+		CodedOrFreeTextValue codedValue = converter.convert(CONCEPT_NAME_PREFIX + conceptNameId);
+		ConceptName name = codedValue.getCodedNameValue();
 		assertEquals(conceptNameId, name.getConceptNameId().intValue());
 	}
 	
 	/**
 	 * @verifies convert a valid concept name uuid string
-	 * @see StringToCodedValueOrFreeTextConverter#convert(String)
+	 * @see StringToCodedOrFreeTextValueConverter#convert(String)
 	 */
 	@Test
 	public void convert_shouldConvertAValidConceptNameUuidString() throws Exception {
 		String conceptNameUuid = "9bc5693a-f558-40c9-8177-145a4b119ca7";
-		String toConvert = StringToCodedValueOrFreeTextConverter.CONCEPT_NAME_PREFIX + conceptNameUuid;
-		CodedValueOrFreeText codedValue = converter.convert(toConvert);
-		ConceptName name = codedValue.getValueAsType();
+		CodedOrFreeTextValue codedValue = converter.convert(CONCEPT_NAME_PREFIX + conceptNameUuid);
+		ConceptName name = codedValue.getCodedNameValue();
 		assertEquals(conceptNameUuid, name.getUuid());
 	}
 	
 	/**
 	 * @verifies handle free text
-	 * @see StringToCodedValueOrFreeTextConverter#convert(String)
+	 * @see StringToCodedOrFreeTextValueConverter#convert(String)
 	 */
 	@Test
 	public void convert_shouldHandleFreeText() throws Exception {
 		String text = "some random text";
-		CodedValueOrFreeText codedValue = converter.convert(text);
-		assertEquals(text, codedValue.getValueAsType());
+		CodedOrFreeTextValue codedValue = converter.convert(text);
+		assertEquals(text, codedValue.getValue());
 	}
 	
 	/**
 	 * @verifies not return null
-	 * @see StringToCodedValueOrFreeTextConverter#convert(String)
+	 * @see StringToCodedOrFreeTextValueConverter#convert(String)
 	 */
 	@Test
 	public void convert_shouldNotReturnNull() throws Exception {
 		String conceptUuid = "some invalid concept uuid";
-		String toConvert = StringToCodedValueOrFreeTextConverter.CONCEPT_PREFIX + conceptUuid;
-		CodedValueOrFreeText codedValue = converter.convert(toConvert);
+		CodedOrFreeTextValue codedValue = converter.convert(CONCEPT_PREFIX + conceptUuid);
 		assertNotNull(codedValue);
 		assertNull(codedValue.getValue());//the wrapped value can be null
 	}
