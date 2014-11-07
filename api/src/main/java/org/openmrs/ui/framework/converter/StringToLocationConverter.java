fb23faff87ab0d01ef8af2d84adc65bf9c125d21
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-11-06 16:42:02 -0800

    UIFR-148 - LocationConverter should support finding a location by id or uuids

--- api/src/main/java/org/openmrs/ui/framework/converter/StringToLocationConverter.java
+++ api/src/main/java/org/openmrs/ui/framework/converter/StringToLocationConverter.java
@@ -2,16 +2,25 @@ package org.openmrs.ui.framework.converter;
 
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.Location;
-import org.openmrs.api.context.Context;
+import org.openmrs.api.LocationService;
+import org.openmrs.ui.framework.converter.util.ConversionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 
 public class StringToLocationConverter implements Converter<String, Location> {
-	
-	@Override
-	public Location convert(String locationId) {
-		if (StringUtils.isBlank(locationId))
-			return null;
-		return Context.getLocationService().getLocation(Integer.valueOf(locationId));
+
+    @Autowired
+    private LocationService locationService;
+
+    @Override
+	public Location convert(String id) {
+        if (StringUtils.isBlank(id)) {
+            return null;
+        } else if (ConversionUtil.onlyDigits(id)) {
+            return locationService.getLocation(Integer.valueOf(id));
+        } else {
+            return locationService.getLocationByUuid(id);
+        }
 	}
 	
 }
