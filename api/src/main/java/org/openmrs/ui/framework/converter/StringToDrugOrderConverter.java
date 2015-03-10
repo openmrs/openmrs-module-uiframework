25cd065345c50fe3709c7a0d9b575b0fe931953a
Darius Jazayeri <jazayeri@alum.mit.edu>
2015-03-09 23:54:32 -0700

    UIFR-155 - Converters for Order and DrugOrder

---
+++ api/src/main/java/org/openmrs/ui/framework/converter/StringToDrugOrderConverter.java
@@ -0,0 +1,27 @@
+package org.openmrs.ui.framework.converter;
+
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.DrugOrder;
+import org.openmrs.api.OrderService;
+import org.openmrs.ui.framework.converter.util.ConversionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToDrugOrderConverter implements Converter<String, DrugOrder> {
+
+    @Autowired
+    @Qualifier("orderService")
+    private OrderService service;
+
+    @Override
+    public DrugOrder convert(String id) {
+        if (StringUtils.isBlank(id)) {
+            return null;
+        } else if (ConversionUtil.onlyDigits(id)) {
+            return service.getOrder(Integer.valueOf(id), DrugOrder.class);
+        } else {
+            return (DrugOrder) service.getOrderByUuid(id);
+        }
+    }
+}
---
+++ api/src/main/java/org/openmrs/ui/framework/converter/StringToOrderConverter.java
@@ -0,0 +1,28 @@
+package org.openmrs.ui.framework.converter;
+
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.Order;
+import org.openmrs.api.OrderService;
+import org.openmrs.api.PatientService;
+import org.openmrs.ui.framework.converter.util.ConversionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToOrderConverter implements Converter<String, Order> {
+
+    @Autowired
+    @Qualifier("orderService")
+    private OrderService service;
+
+    @Override
+    public Order convert(String id) {
+        if (StringUtils.isBlank(id)) {
+            return null;
+        } else if (ConversionUtil.onlyDigits(id)) {
+            return service.getOrder(Integer.valueOf(id));
+        } else {
+            return service.getOrderByUuid(id);
+        }
+    }
+}
