b33970c3dac2e7ee0994bc55068670322a582a73
mgoodrich <mark.goodrich@live.com>
2020-04-15 15:30:39 -0400

    UFIR-214: Allow including CSS and JS files not in scripts and styles subdirectory

--- api/src/main/java/org/openmrs/ui/framework/UiUtils.java
+++ api/src/main/java/org/openmrs/ui/framework/UiUtils.java
@@ -1,14 +1,5 @@
 package org.openmrs.ui.framework;
 
-import java.io.StringWriter;
-import java.text.SimpleDateFormat;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Random;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -30,68 +21,81 @@ import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
 
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Utility methods that should be available in view technologies for pages and fragments
  */
 public abstract class UiUtils {
-	
+
 	protected Locale locale;
-	
+
 	protected PageContext pageContext;
-	
+
 	protected ResourceIncluder resourceIncluder;
-	
+
 	protected FragmentIncluder fragmentIncluder;
-	
+
 	protected Formatter formatter;
-	
+
 	protected Messager messager;
-	
+
 	protected Decoratable decoratable;
-	
+
 	protected ExtensionManager extensionManager;
-	
+
 	protected ConversionService conversionService;
-	
+
 	public void includeCss(String file) {
 		includeCss(null, file, null);
 	}
-	
+
 	public void includeCss(String providerName, String file) {
 		includeCss(providerName, file, null);
 	}
-	
-	public void includeCss(String providerName, String file, Integer priority) {
+
+	public void includeCss(String providerName, String file, Integer priority) { includeCss(providerName, file, priority, false);}
+
+	public void includeCss(String providerName, String file, Integer priority, Boolean excludeStylesPrefixFromPath) {
 		MapResourceExtension resource = mapResource(providerName, file);
-		
-		resourceIncluder.includeResource(new Resource(Resource.CATEGORY_CSS, resource.getProviderId(), "styles/"
-		        + resource.getResourceId(), priority));
+
+		resourceIncluder.includeResource(new Resource(Resource.CATEGORY_CSS, resource.getProviderId(),
+                (excludeStylesPrefixFromPath ? "" : "styles/") + resource.getResourceId(), priority));
 	}
-	
+
 	public void includeJavascript(String file) {
 		includeJavascript(null, file, null);
 	}
-	
+
 	public void includeJavascript(String providerName, String file) {
 		includeJavascript(providerName, file, null);
 	}
-	
-	public void includeJavascript(String providerName, String file, Integer priority) {
+
+    public void includeJavascript(String providerName, String file, Integer priority) { includeJavascript(providerName, file, priority, false);}
+
+	public void includeJavascript(String providerName, String file, Integer priority, Boolean excludeScriptsPrefixFromPath) {
 		MapResourceExtension resource = mapResource(providerName, file);
-		
-		resourceIncluder.includeResource(new Resource(Resource.CATEGORY_JS, resource.getProviderId(), "scripts/"
-		        + resource.getResourceId(), priority));
+
+		resourceIncluder.includeResource(new Resource(Resource.CATEGORY_JS, resource.getProviderId(),
+                (excludeScriptsPrefixFromPath ? "" : "scripts/") + resource.getResourceId(), priority));
 	}
-	
+
 	/**
 	 * Generates HTML resource linkages for all resources requested by the page the fragments on
 	 * this request
-	 * 
+	 *
 	 * @return the html
 	 */
 	public String resourceLinks() {
 		StringBuilder ret = new StringBuilder();
-		
+
 		// Include all Javascript resources
 		for (Resource resource : pageContext.uniqueSortedResourcesByCategory(Resource.CATEGORY_JS)) {
 			ret.append("<script type=\"text/javascript\" src=\"/");
@@ -101,7 +105,7 @@ public abstract class UiUtils {
 			ret.append("?cache=" + UiFrameworkActivator.getContextLastRefreshedTime());
 			ret.append("\"></script>\n");
 		}
-		
+
 		// Include all CSS resources
 		for (Resource resource : pageContext.uniqueSortedResourcesByCategory(Resource.CATEGORY_CSS)) {
 			ret.append("<link rel=\"stylesheet\" href=\"/");
@@ -111,65 +115,65 @@ public abstract class UiUtils {
 			ret.append("?cache=" + UiFrameworkActivator.getContextLastRefreshedTime());
 			ret.append("\" type=\"text/css\"/>\n");
 		}
-		
+
 		return ret.toString();
 	}
-	
+
 	public String thisUrl() {
 		// TODO determine whether we still need both this method and thisUrlWithContextPath()
 		return pageContext.getUrl(true);
 	}
-	
+
 	public String thisUrlWithContextPath() {
 		return pageContext.getUrl(true);
 	}
-	
+
 	public String startForm(String providerName, String fragment, String action) {
 		return startForm(providerName, fragment, action, null);
 	}
-	
+
 	public String startForm(String providerName, String fragment, String action, Map<String, CharSequence> parameters) {
 		StringBuilder ret = new StringBuilder();
 		String url = actionLink(providerName, fragment, action, parameters);
 		ret.append("<form method='post' action='" + url + "'>");
 		return ret.toString();
 	}
-	
+
 	public String endForm() {
 		return "</form>";
 	}
-	
+
 	public String message(String code, Object... args) {
 		if (StringUtils.isBlank(code)) {
 			return "";
 		}
 		return messager.message(code, args);
 	}
-	
+
 	public String includeFragment(String providerName, String fragmentId) throws PageAction {
 		MapResourceExtension resource = mapResource(providerName, fragmentId);
-		
+
 		return fragmentIncluder.includeFragment(new FragmentRequest(resource.getProviderId(), resource.getResourceId()));
 	}
-	
+
 	public String includeFragment(String providerName, String fragmentId, Map<String, Object> config) throws PageAction {
 		MapResourceExtension resource = mapResource(providerName, fragmentId);
-		
+
 		return fragmentIncluder.includeFragment(new FragmentRequest(resource.getProviderId(), resource.getResourceId(),
 		        config));
 	}
-	
+
 	public void decorateWith(String providerName, String fragmentId) {
 		decorateWith(providerName, fragmentId, null);
 	}
-	
+
 	public void decorateWith(String providerName, String fragmentId, Map<String, Object> config) {
 		MapResourceExtension resource = mapResource(providerName, fragmentId);
-		
+
 		decoratable.setDecorateWith(new FragmentRequest(resource.getProviderId(), "decorator/" + resource.getResourceId(),
 		        config));
 	}
-	
+
 	private MapResourceExtension mapResource(String providerName, String fragmentId) {
 		Collection<MapResourceExtension> extensions = extensionManager.getExtensions(MapResourceExtension.class,
 		    UiFrameworkConstants.MAP_RESOURCE_EXTENSION_POINT_ID);
@@ -178,18 +182,18 @@ public abstract class UiUtils {
 				return extension;
 			}
 		}
-		
+
 		//no mapping
 		MapResourceExtension resource = new MapResourceExtension();
 		resource.setResourceId(fragmentId);
 		resource.setProviderId(providerName);
 		return resource;
 	}
-	
+
 	public String decorate(String providerName, String decoratorName, String contents) throws PageAction {
 		return decorate(providerName, decoratorName, null, contents);
 	}
-	
+
 	public String decorate(String providerName, String decoratorName, Map<String, Object> decoratorConfig, String contents)
 	        throws PageAction {
 		if (decoratorConfig == null)
@@ -197,15 +201,15 @@ public abstract class UiUtils {
 		decoratorConfig.put("content", contents);
 		return includeFragment(providerName, "decorator/" + decoratorName, decoratorConfig);
 	}
-	
+
 	public String contextPath() {
 		return WebConstants.CONTEXT_PATH;
 	}
-	
+
 	public String resourceLink(String path) {
 		return resourceLink(null, path);
 	}
-	
+
 	public String resourceLink(String providerName, String path) {
 		if (providerName == null)
 			providerName = "*";
@@ -216,14 +220,14 @@ public abstract class UiUtils {
 		sb.append(path);
 		return sb.toString();
 	}
-	
+
 	public String pageLink(String providerName, String pageName) {
 		return pageLink(providerName, pageName, null);
 	}
-	
+
 	/**
 	 * Like #pageLink(String, String, Map), but doesn't add the context path at the beginning.
-	 * 
+	 *
 	 * @param providerName
 	 * @param pageName
 	 * @param params
@@ -268,10 +272,10 @@ public abstract class UiUtils {
 			ret += "#" + extraAnchor;
 		return ret;
 	}
-	
+
 	/**
 	 * Supports query parameters and anchors in the pageName, e.g. "myPage?one=1&two=2#mySection"
-	 * 
+	 *
 	 * @param providerName
 	 * @param pageName
 	 * @param params
@@ -284,11 +288,11 @@ public abstract class UiUtils {
 	public String pageLink(String providerName, String pageName, Map<String, Object> params) {
 		return "/" + contextPath() + pageLinkWithoutContextPath(providerName, pageName, params);
 	}
-	
+
 	public String actionLink(String providerName, String controllerName, String action) {
 		return actionLink(providerName, controllerName, action, null);
 	}
-	
+
 	public String actionLink(String providerName, String controllerName, String action, Map<String, ?> args) {
 		if (providerName == null) {
 			providerName = "*";
@@ -311,7 +315,7 @@ public abstract class UiUtils {
 		sb.append("successUrl=" + urlEncode(successUrl));
 		return sb.toString();
 	}
-	
+
 	public Object urlEncode(CharSequence string) {
 		// TODO fix this
 		return java.net.URLEncoder.encode(string.toString());
@@ -341,10 +345,10 @@ public abstract class UiUtils {
 			String key = binding.getKey().replace(" ", "");
 			url = url.replace("{{" + key + "}}", "" + binding.getValue());
 		}
-		
+
 		return url;
 	}
-	
+
 	public String dateToString(Date date) {
 		return new SimpleDateFormat(WebConstants.DATE_FORMAT_TIMESTAMP).format(date);
 	}
@@ -364,7 +368,7 @@ public abstract class UiUtils {
 	 * Formats the specified date to a string using the specified format and drops the time
 	 * component, the text 'Today' is returned, if the date matches the previous date or the text
 	 * Yesterday is returned if the date matches the previous day of the year.
-	 * 
+	 *
 	 * @param date the date to format
 	 * @should replace the current date with today text
 	 * @should replace the previous date with yesterday
@@ -372,7 +376,7 @@ public abstract class UiUtils {
 	public String formatDatePretty(Date date) {
 		DateExt dateExt = new DateExt(date);
 		Date today = new Date();
-		
+
 		if (dateExt.isSameDay(today)) {
 			return message("uiframework.today");
 		} else if (dateExt.isDayBefore(today)) {
@@ -381,25 +385,25 @@ public abstract class UiUtils {
 			return format(dateExt.getDateWithoutTime());
 		}
 	}
-	
+
 	/**
 	 * Formats a date with this format: dd MMM yyyy hh:mm a
-	 * 
+	 *
 	 * @param date the date to format
 	 */
 	public String formatDatetimePretty(Date date) {
 		return formatDatePretty(date) + " " + DateFormatUtils.format(date, "hh:mm a", locale);
 	}
-	
+
 	public String format(Object o) {
 		return formatter.format(o, getLocale());
 	}
-	
+
 	public String formatTimeAgo(Date date) {
 		long diff = System.currentTimeMillis() - date.getTime();
 		return message("duration.secondsAgo", (diff / 1000));
 	}
-	
+
 	/**
 	 * @param o
 	 * @return
@@ -409,16 +413,16 @@ public abstract class UiUtils {
 	public String formatAsText(Object o) {
 		return formatter.format(o, getLocale());
 	}
-	
+
 	public static String randomId(String prefix) {
 		if (prefix == null)
 			prefix = "id";
 		return prefix + new Random().nextInt(10000);
 	}
-	
+
 	/**
 	 * Used to make sure that a value can safely compile and be used inside a JavaScript string
-	 * 
+	 *
 	 * @param input the input text which may have characters that JavaScript requires to escape
 	 * @return text that is fit to be used as a JavaScript string
 	 */
@@ -434,63 +438,63 @@ public abstract class UiUtils {
 
 	/**
 	 * Used to make sure that a string can safely be used in a URL
-	 * 
+	 *
 	 * @param input the input text which may have characters that are URL unsafe
 	 * @return text that is fit to be used in an URL
 	 */
 	public String encodeForSafeURL(String input) {
 		return Encode.forUriComponent(input);
 	}
-	
+
 	/**
 	 * @see org.owasp.encoder.Encode#forJavaScriptAttribute(java.lang.String)
 	 */
 	public String encodeJavaScriptAttribute(String input) {
 		return Encode.forJavaScriptAttribute(input);
 	}
-	
+
 	/**
 	 * @see org.owasp.encoder.Encode#forJavaScript(java.lang.String)
 	 */
 	public String encodeJavaScript(String input) {
 		return Encode.forJavaScript(input);
 	}
-	
+
 	/**
 	 * @see org.owasp.encoder.Encode#forJavaScriptBlock(java.lang.String)
 	 */
 	public String encodeJavaScriptBlock(String input) {
 		return Encode.forJavaScriptBlock(input);
 	}
-	
+
 	/**
 	 * @see org.owasp.encoder.Encode#forJavaScriptSource(java.lang.String)
 	 */
 	public String encodeJavaScriptSource(String input) {
 		return Encode.forJavaScriptSource(input);
 	}
-	
+
 	/**
 	 * @see org.owasp.encoder.Encode#forHtmlContent(java.lang.String)
 	 */
 	public String encodeHtmlContent(String input) {
 		return Encode.forHtmlContent(input);
 	}
-	
+
 	/**
 	 * @see org.owasp.encoder.Encode#forHtmlAttribute(java.lang.String)
 	 */
 	public String encodeHtmlAttribute(String input) {
 		return Encode.forHtmlAttribute(input);
 	}
-	
+
 	/**
 	 * @see org.owasp.encoder.Encode#forHtml(java.lang.String)
 	 */
 	public String encodeHtml(String input) {
 		return Encode.forHtml(input);
 	}
-	
+
 	public String escapeAttribute(String input) {
 		if (input == null) {
 			return null;
@@ -498,7 +502,7 @@ public abstract class UiUtils {
 		input = input.replaceAll("\"", "&quot;");
 		return input;
 	}
-	
+
 	public String escapeHtml(String input) {
 		if (input == null) {
 			return null;
@@ -507,29 +511,29 @@ public abstract class UiUtils {
 		input = input.replaceAll(">", "&gt;");
 		return input;
 	}
-	
+
 	public ExtensionManager getExtensionManager() {
 		return extensionManager;
 	}
-	
+
 	public <T> T convert(Object fromValue, Class<T> clazz) {
 		return conversionService.convert(fromValue, clazz);
 	}
-	
+
 	/**
 	 * Simplifies a complex object into a simple object suitable for serialization to JSON
-	 * 
+	 *
 	 * @param complex the complex object
 	 * @return the simplified representation
 	 */
 	public SimpleObject simplifyObject(Object complex) {
 		return convert(complex, SimpleObject.class);
 	}
-	
+
 	/**
 	 * Simplifies a collection of complex objects into an array of simple objects suitable for
 	 * serialization to JSON
-	 * 
+	 *
 	 * @param complex the collection of complex objects
 	 * @return the simplified representation
 	 */
@@ -537,10 +541,10 @@ public abstract class UiUtils {
 		if (complex.size() == 0) {
 			return new SimpleObject[] {};
 		}
-		
+
 		return convert(complex, SimpleObject[].class);
 	}
-	
+
 	public String toJson(Object object) {
 		try {
 			if (object instanceof ObjectResult) {
@@ -555,11 +559,11 @@ public abstract class UiUtils {
 			throw new UiFrameworkException("Error generating JSON", ex);
 		}
 	}
-	
+
 	/**
 	 * Validates target with validator. If any errors are found, this throws an exception which can
 	 * be caught by the UI Framework
-	 * 
+	 *
 	 * @param target
 	 * @param validator
 	 * @param bindingPrefix
@@ -571,14 +575,14 @@ public abstract class UiUtils {
 		if (result.hasErrors())
 			throw new BindParamsValidationException(bindingPrefix, result);
 	}
-	
+
 	/**
 	 * @return the configured locale, or Context.getLocale() if none is set
 	 */
 	public Locale getLocale() {
 		return Context.getLocale();
 	}
-	
+
 	public void setLocale(Locale locale) {
 		this.locale = locale;
 	}
