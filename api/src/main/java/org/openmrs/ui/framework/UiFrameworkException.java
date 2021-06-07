c0a3cd640cc6180ed0991b0373821605ff3c2bdd
Vivek Singh <petmongrels@gmail.com>
2021-06-07 17:31:02 +0530

    Encoded the exception message so that the JavaScript cannot be inserted into it via the request parameters. Sometimes the request parameters are sent back in the error response. e.g. /openmrs/coreapp%3Cimg%20src=a%20onerror=alert(1)%3E/findpatient/findPatient.page

--- api/src/main/java/org/openmrs/ui/framework/UiFrameworkException.java
+++ api/src/main/java/org/openmrs/ui/framework/UiFrameworkException.java
@@ -1,19 +1,24 @@
 package org.openmrs.ui.framework;
 
+import org.owasp.encoder.Encode;
+
 public class UiFrameworkException extends RuntimeException {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public UiFrameworkException() {
 		super();
 	}
-	
+
 	public UiFrameworkException(String message) {
-		super(message);
+		super(getEncodedMessage(message));
 	}
-	
+
 	public UiFrameworkException(String message, Throwable throwable) {
-		super(message, throwable);
+		super(getEncodedMessage(message), throwable);
 	}
-	
+
+	private static String getEncodedMessage(String message) {
+		return message == null ? null : Encode.forHtml(message);
+	}
 }
